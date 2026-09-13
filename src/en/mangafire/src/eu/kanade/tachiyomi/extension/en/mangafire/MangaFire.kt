02d7cb81f501d205d15d0f60e7f92a3f25e54266
WavyKisuke <p94rnfbfrc@privaterelay.appleid.com>
2026-09-12 20:06:56 -0700

    Trigger TachiManga repository republish

--- src/en/mangafire/src/eu/kanade/tachiyomi/extension/en/mangafire/MangaFire.kt
+++ src/en/mangafire/src/eu/kanade/tachiyomi/extension/en/mangafire/MangaFire.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.mangafire
 
+// Keep this source touch in the publish path so repository metadata fixes are rebuilt.
 import androidx.preference.MultiSelectListPreference
 import androidx.preference.PreferenceScreen
 import androidx.preference.SwitchPreferenceCompat
